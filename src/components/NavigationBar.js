a1c44bd6bc61b3a475cbc871fd9a59b6c413e944
Kgotso Bapela <kgotso.bapela@rain.co.za>
2024-10-10 14:39:40 +0200

    Back-end deployed

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,29 +1,17 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { LinkContainer } from 'react-router-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap';
+import cclogo from '../components/images/cclogo-nobg.png';
+import '../App.css';
 
 function NavigationBar() {
-//   return (
-//     <Navbar expand="lg" className="bg-body-tertiary">
-//       <Container>
-//         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//         <Navbar.Collapse id="basic-navbar-nav">
-//           <Nav className="me-auto">
-//             <Nav.Link href="#home">Home</Nav.Link>
-//             <Nav.Link href="#link">Link</Nav.Link>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   );
 
 return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="myColor" expand="lg" sticky="top" >
       <Container>
         <LinkContainer to="/">
-          <Navbar.Brand>Bike Borrowing</Navbar.Brand>
+          <Navbar.Brand href="/"><img src={cclogo} width="50" height="50" alt=" CC Logo" /> <span className="brand-text">Community Connect</span></Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = "https://github.com/kgotsoBapela/communityconnect-backup.git/api";
+const API_BASE_URL = "https://communityconnect-cleardb-b9c1eb0cd158.herokuapp.com/api";
 
 export default API_BASE_URL;
