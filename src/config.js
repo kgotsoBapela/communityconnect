5cffe8e5c23c5b3b584090d489ba2f87b19b1d94
Kgotso Bapela <kgotso.bapela@rain.co.za>
2025-01-24 12:33:28 +0200

    Online deployment for Julian

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "https://communityconnect-cleardb-b9c1eb0cd158.herokuapp.com/api";
-const API_BASE_URL = "http://localhost:5000/api";
-// const API_BASE_URL = "https://communityconnect-backend.onrender.com/api";
+// const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://communityconnect-backend.onrender.com/api";
 
 export default API_BASE_URL;
