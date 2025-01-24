11819d479d4d0216a2642997fa1adff17633e18a
Kgotso Bapela <kgotso.bapela@rain.co.za>
2025-01-24 10:35:40 +0200

    First commit 2025

--- src/components/BikeCard.js
+++ src/components/BikeCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
-import BorrowButton from './BorrowButton';
+// import BorrowButton from './BorrowButton';
 // import Login from './Login';
 
 const BikeCard = ({ bike, userRole, handleShowLogin }) => {
